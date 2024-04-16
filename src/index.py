cf0d6023314601a97006ee8583ed1fd990a93e30
miahro <mikko.ahro@gmail.com>
2024-04-16 12:26:38 +0300

    initial commit

---
+++ src/index.py
@@ -0,0 +1,10 @@
+"""Module for main entry point for the application."""
+
+
+def main():
+    """Main entry point for the application."""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
